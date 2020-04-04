5ae8da2ace70160c94a8c371639bb603ae273dc3
uvsq21918311 <elkadi96hotmail.com>
2020-04-04 18:27:25 +0200

    Add class PersonnelType

---
+++ src/main/java/fr/uvsq/pglp_5_2/PersonnelType.java
@@ -0,0 +1,12 @@
+package fr.uvsq.pglp_5_2;
+
+import java.io.Serializable;
+
+public abstract class PersonnelType implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	abstract public boolean isGroupe();
+
+}
