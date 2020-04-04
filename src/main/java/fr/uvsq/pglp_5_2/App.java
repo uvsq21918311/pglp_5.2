62577fcd6fc92674395fa4e4c593be1088b6f8be
uvsq21918311 <elkadi96hotmail.com>
2020-04-04 16:32:35 +0200

    first commit

---
+++ src/main/java/fr/uvsq/pglp_5_2/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.pglp_5_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
